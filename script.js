29562764a823691e652ed3e6eb9741c9625205d5
babelhead <72417587+babelhead@users.noreply.github.com>
2021-04-11 15:36:55 +0200

    amend script

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let hello = document.getElementById("b1");
-hello.addEventListener("click", whatsYourName);
-function whatsYourName() {
-    prompt("Hello there! What's your name?");
-    let para = document.createElement("p");
-    para.textContent = "Thanks for popping by" + whatsYourName + "! I hope you'll find some of this interesting!"
-    document.body.appendChild(para); 
+hello.addEventListener("click", display);
+
+function display() {
+let name = prompt("Hello there! What's your name?");
+let para = document.createElement("p");
+  para.textContent = "Thanks for popping by, " + name + "! I hope you'll find some of this interesting!"
+  document.body.appendChild(para);
 }
