718c36609c6c740681a07c5bf66d3e92597e0606
babelhead <72417587+babelhead@users.noreply.github.com>
2021-04-11 12:50:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let hello = document.getElementById("b1");
+let whatsYourName
 hello.addEventListener("click", whatsYourName);
-function whatsYourName() {
-    prompt("Hello there! What's your name?");
-}
+whatsYourName = prompt("Hello there! What's your name?");
 let para = document.createElement("p");
 para.textContent = "Thanks for popping by" + whatsYourName + "! I hope you'll find some of this interesting!"
 document.body.appendChild(para); 
