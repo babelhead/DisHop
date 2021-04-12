8bc95352aa2c44dbc1dbf36c479ff018f5a2a629
babelhead <72417587+babelhead@users.noreply.github.com>
2021-04-12 10:09:37 +0200

    style and script update

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ hello.addEventListener("click", display);
 function display() {
 let name = prompt("Hello there! What's your name?");
 let para = document.createElement("p");
-  para.textContent = "Thanks for popping by, " + name + "! I hope you'll find some of this interesting!"
+  if(name == null) {
+  para.textContent = "OK, you might not want to tell me your name, but thanks anyway for popping by! I hope you'll find some of this interesting."
   document.body.appendChild(para);
+  } else {
+    para.textContent = "Thanks for popping by, " + name + "! I hope you'll find some of this interesting!"
+    document.body.appendChild(para);
+  }
 }
