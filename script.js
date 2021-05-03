71d1e13a055da6e749ea798a2ef0d5c05ed18b77
babelhead <72417587+babelhead@users.noreply.github.com>
2021-05-03 17:08:40 +0200

    generator

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ let para = document.createElement("p");
     document.body.appendChild(para);
   }
 }
+
+function genereTexte() {
+  let verbes = ["frotte", "détends", "épluche", "caresse", "déplie", "découpe", "raffine", "lave", "branche", "touche"];
+  let objecs = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller"];
+  let verbe = Math.round(Math.random() * 10);
+  let objet = Math.round(Math.random() * 10);
+  return(verbes[verbe] + "-moi " + objects[object]);
+  }
+
+function display() {
+  let texte = genereTexte();
+  let para = document.createElement("p");
+  para.textContent = texte;
+  document.body.appendChild(para);
+  }
