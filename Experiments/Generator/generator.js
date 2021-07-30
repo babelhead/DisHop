c36ef24aa1ab7a51eff3f41d7747b6e2d8c35100
babelhead <72417587+babelhead@users.noreply.github.com>
2021-07-30 08:12:46 +0200

    JS update

--- Experiments/Generator/generator.js
+++ Experiments/Generator/generator.js
@@ -1,13 +1,22 @@
-function genereTexte() {
-    let verbes = ["frotte", "détends", "épluche", "caresse", "déplie", "découpe", "raffine", "lave", "branche", "touche"];
-    let objecs = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller"];
-    let verbe = Math.round(Math.random() * 10);
-    let objet = Math.round(Math.random() * 10);
-    return(verbes[verbe] + "-moi " + objects[object]);
-}
-function display() {
-    let texte = genereTexte();
-    let para = document.createElement("p");
-    para.textContent = texte;
-    document.body.appendChild(para);
-}
+let ajouter = document.querySelector("#b1")//getElementById("b1");
+            ajouter.addEventListener('click', ajout);
+            function ajout() {
+                let para = document.createElement('p');
+                para.textContent = genereTexte(); 
+                /*function genereTexte() {
+                let verbes = ["Frotte", "Détends", "Épluche", "Caresse", "Déplie", "Découpe", "Raffine", "Lave", "Branche", "Touche", "Gratte"];
+                let objets = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller", "la grappe"];
+                let verbe = Math.round(Math.random() * 10);
+                let objet = Math.round(Math.random() * 10);
+                return(verbes[verbe] + "-moi " + objets[objet]);
+                } */
+                document.body.appendChild(para);
+            }
+            
+  function genereTexte() {
+  let verbes = ["frotte", "détends", "épluche", "caresse", "déplie", "découpe", "raffine", "lave", "branche", "touche"];
+  let objects = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller"];
+  let verbe = Math.round(Math.random() * 10);
+  let object = Math.round(Math.random() * 10);
+  return(verbes[verbe] + "-moi " + objects[object]);
+  }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let hello = document.getElementById("b1");
+/* let hello = document.getElementById("b1");
 hello.addEventListener("click", display);
 
 function display() {
@@ -11,19 +11,34 @@ let para = document.createElement("p");
     para.textContent = "Thanks for popping by, " + name + "! I hope you'll find some of this interesting!"
     document.body.appendChild(para);
   }
-}
+} */
 
-function genereTexte() {
+let ajouter = document.getElementById("b1");
+            ajouter.addEventListener('click', ajout);
+            function ajout() {
+                let para = document.createElement('p');
+                para.textContent = genereTexte(); 
+                /*function genereTexte() {
+                let verbes = ["Frotte", "Détends", "Épluche", "Caresse", "Déplie", "Découpe", "Raffine", "Lave", "Branche", "Touche", "Gratte"];
+                let objets = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller", "la grappe"];
+                let verbe = Math.round(Math.random() * 10);
+                let objet = Math.round(Math.random() * 10);
+                return(verbes[verbe] + "-moi " + objets[objet]);
+                } */
+                document.body.appendChild(para);
+            }
+            
+  function genereTexte() {
   let verbes = ["frotte", "détends", "épluche", "caresse", "déplie", "découpe", "raffine", "lave", "branche", "touche"];
-  let objecs = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller"];
+  let objects = ["la carotte", "le chocolat", "les narines", "les mots d'amour", "les haricots", "la patate", "l'oignon", "la culotte", "la bouteille", "l'oreiller"];
   let verbe = Math.round(Math.random() * 10);
-  let objet = Math.round(Math.random() * 10);
+  let object = Math.round(Math.random() * 10);
   return(verbes[verbe] + "-moi " + objects[object]);
   }
 
-function display() {
+/* function display() {
   let texte = genereTexte();
   let para = document.createElement("p");
   para.textContent = texte;
   document.body.appendChild(para);
-  }
+  } */
